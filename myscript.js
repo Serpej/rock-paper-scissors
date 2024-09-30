7396946d2fc4e6bb28dcd3cf39a81fabb42648a4
Serpej <Jehag@ive.se>
2024-09-30 17:13:11 +0200

    Add function getComputerChoice
    chooses either Rock Paper or Scissors randomly and
    prints it in the console.

---
+++ myscript.js
@@ -0,0 +1,33 @@
+console.log("Hello World!");
+
+/* #Create a function called getComputerChoice;
+
+#Use Math.random to randomly choose a number between 0 and 1;
+
+#Create conditionals in the getComputerChoice function, which returns Rock Paper or Scissors depending on the randomly generated number.
+ Each number responds to a third of the decimals between 0 and 1; */
+
+ function getComputerChoice() {
+    
+    const choice = Math.random();
+    
+    if (choice <= 0.33) {
+        return console.log("Rock");
+
+    } else if (choice >= 0.66) {
+        return console.log("Scissors");
+
+    } else {
+       return console.log("Paper");
+    }
+ }
+
+ getComputerChoice()
+ getComputerChoice()
+ getComputerChoice()
+ getComputerChoice()
+ getComputerChoice()
+ getComputerChoice()
+
+    
+  
