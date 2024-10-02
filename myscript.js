e0f0ff725e8c4929031815680f49df219699b173
Serpej <Jehag@ive.se>
2024-10-02 20:07:11 +0200

    Add playRound() and score variables
    I still have some trouble with the function
    arguments and parameters. The code doesn't
    recognize all the choices, even though the code
    still runs.

--- myscript.js
+++ myscript.js
@@ -12,39 +12,80 @@ console.log("Hello World!");
     const choice = Math.random();
     
     if (choice <= 0.33) {
-        return console.log("Rock");
+        return console.log("You chose Rock.");
 
     } else if (choice >= 0.66) {
-        return console.log("Scissors");
+        return console.log("You chose Scissors.");
 
     } else {
-       return console.log("Paper");
+       return console.log("You chose Paper.");
     }
  }
 
  getComputerChoice()
 
- function getHumanChoice() {
+  function getHumanChoice() {
    let choice = window.prompt("Rock, Paper or Scissors?");
 
-   if (choice.toLowerCase() === "rock") {
-    return console.log("Rock");
+   if (choice === null) {
+      return console.log("Come on, let's play!");
 
    } else if (choice.toLowerCase() === "paper") {
-    return console.log("Paper");
+    return "Paper";
    
    } else if (choice.toLowerCase() === "scissors") {
     return console.log("Scissors");
-   } 
+   
+   } else if (choice.toLowerCase() === "rock") {
+      return console.log("Rock");
+   }
  }
 
  getHumanChoice()
 
- /* #Create a function that calls getComputerChoice() when we write either Rock Paper or Scissors in the console.
 
-    #Create a function that keeps score by increasing numbers and associates them with computer and player.
 
-    #Create a function that checks "who wins" with conditionals, prints the score and an appropriate message.*/
+
+ /* #Create a function that keeps score by increasing numbers and associates them with computer and player. */
+
+ let humanScore = 0
+ let computerScore = 0
+
+/*  #Create a function with the human choice and the computer choice as arguments, plays a single round and increases the winners score by one */
+
+function playRound (choice1, choice2) {
+   
+/*    if (choice === null){
+      console.log("Come on, just one more round!"), getHumanChoice(); */
+
+   if (choice1 === "Paper" && choice2 === "Rock") {
+      ++humanScore, console.log(`You win this round! Human score:{$humanScore} Computer score:{$computerScore}`);
+
+   } else if (choice1 === "Rock" && choice2 === "Scissors") {
+      ++humanScore, console.log(`You win this round! Human score:{$humanScore} Computer score:{$computerScore}`);
+   
+   } else if (choice1 === "Scissors" && choice2 === "Paper") {
+      ++humanScore, console.log(`You win this round! Human score:{$humanScore} Computer score:{$computerScore}`);
+
+   } else if (choice1 === choice2) {
+      console.log(`It's a tie. Human score:${humanScore} Computer score:${computerScore}`);
+
+   } else { ++computerScore, console.log(`Computer wins this round. Human score:${humanScore} Computer score:${computerScore}` );}
+}
+
+playRound(getHumanChoice(), getComputerChoice())
+
+/* if getHumanChoice equals Paper and getComputerChoice equals Rock, increase humanScore by one and print "You won this round!"
+   if getHumanChoice equals Rock and getComputerChoice equals Scissors, increase humanScore by one and print "You won this round!"
+   if getHumanChoice equals Scissors and getComputerChoice equals Paper, increase humanScore by one and print "You won this round!"
+   
+   if getHumanChoice equals getComputerChoice, return "It's a tie"
+   
+   otherwise, increase getComputerChoice by one and print "The computer won this round!*/
+
+
+
+   /*  #Create a function that checks "who wins" with conditionals, prints the score and an appropriate message. */
 
     
   
