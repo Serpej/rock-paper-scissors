fbc0983b81fc8f38f8fdbd80d45fba776273b28e
Serpej <Jehag@ive.se>
2024-10-02 21:33:26 +0200

    Edit arguments in playRound(), return statements in getHumanChoice() and getComputerChoice()
    Can't get the functions to read the conditionals
    correctly.
    
    also the ++computerScore doesn't work, while the
    ++humanScore does?

--- myscript.js
+++ myscript.js
@@ -1,58 +1,51 @@
-console.log("Hello World!");
 
-/* #Create a function called getComputerChoice;
-
-#Use Math.random to randomly choose a number between 0 and 1;
-
-#Create conditionals in the getComputerChoice function, which returns Rock Paper or Scissors depending on the randomly generated number.
- Each number responds to a third of the decimals between 0 and 1; */
-
- function getComputerChoice() {
-    
-    const choice = Math.random();
-    
-    if (choice <= 0.33) {
-        return console.log("The computer chose Rock.");
-
-    } else if (choice >= 0.66) {
-        return console.log("The computer chose Scissors.");
-
-    } else {
-       return console.log("The computer chose Paper.");
-    }
- }
+let Paper = "Paper";
+let Rock = "Rock";
+let Scissors = "Scissors";
 
 
+/* Human Choice */
   function getHumanChoice() {
    let choice = window.prompt("Rock, Paper or Scissors?");
 
    if (choice === null) {
       return console.log("Aw, I wanted to play with you...");
-
-   } else if (choice.toLowerCase() === "paper") {
-    return "You chose Paper.";
    
    } else if (choice.toLowerCase() === "scissors") {
-    return console.log("You chose Scissors.");
+    return Scissors, console.log("You chose Scissors.");
    
    } else if (choice.toLowerCase() === "rock") {
-      return console.log("You chose Rock.");
+      return Rock, console.log("You chose Rock.");
+      
+   } else if (choice.toLowerCase() === "paper") {
+      return Paper, console.log("You chose Paper.");}
    }
- }
 
+   /* Computers choice */
+ function getComputerChoice() {
+    
+   const choice = Math.random();
+  if (getHumanChoice()=== null) {
+     return;
+  
+  } else if (choice <= 0.33) {
+       return Rock, console.log("The computer chose Rock.");
 
+   } else if (choice >= 0.66) {
+       return Scissors, console.log("The computer chose Scissors.");
 
-
- /* #Create a function that keeps score by increasing numbers and associates them with computer and player. */
+   } else {return Paper, console.log("The computer chose Paper.");}
+  }
 
  let humanScore = 0
  let computerScore = 0
 
-/*  #Create a function with the human choice and the computer choice as arguments, plays a single round and increases the winners score by one */
-
+/* Activates the game */
 function playRound (choice1, choice2) {
 
-   let showScore = ` Human score:${humanScore} Computer score:${computerScore}`
+   let showScore = ` Human score:${humanScore} Computer score:${computerScore}`;
+   let anything = "Paper" || "Rock" || "Scissor";
+   
 
    if (choice1 === "Paper" && choice2 === "Rock") {
       ++humanScore, console.log(`You win this round!` + showScore);
@@ -63,12 +56,12 @@ function playRound (choice1, choice2) {
    } else if (choice1 === "Scissors" && choice2 === "Paper") {
       ++humanScore, console.log(`You win this round!` + showScore);
 
+   } else if (choice1 != anything) { 
+      console.log(`Too bad.`);
+   
    } else if (choice1 === choice2) {
       console.log(`It's a tie.` + showScore);
-   
-   }else if (choice1 === null) { 
       
-
    } else { ++computerScore, console.log(`Computer wins this round.` + showScore);}
 }
 
